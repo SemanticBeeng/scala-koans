8d6c20022f2f678377371ea345e719548c22c993
SemanticBeeng <nick@semanticbeeng.com>
2016-01-30 22:19:06 -0800

    AboutTags: todos

--- src/test/scala/org/functionalkoans/forscala/AboutTypeTags.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTypeTags.scala
@@ -81,58 +81,58 @@ class AboutTypeTags extends KoanSuite {
 
   }
 
-  koan("""TypeTags can be used to instantiate types by reflection""") {
-
-    /**
-      * @see http://stackoverflow.com/questions/22970209/get-typetaga-from-classa/22972751#22972751
-      */
-    import scala.reflect._
-
-    class Fruit
-    class Orange extends Fruit
-    class Apple extends Fruit
-    class Banana extends Fruit
-
-    def createNew_1[A <: Fruit](implicit tt: TypeTag[A]) : A = {
-
-      val result = tt.tpe.typeSymbol.getClass match {
-        case o1 if o1.isAssignableFrom(classOf[Orange]) => new Orange()
-        case o2 if o2.isAssignableFrom(classOf[Apple]) => new Apple()
-        case _ => throw new IllegalArgumentException
-      }
-      result.asInstanceOf[A]
-    }
-
-    createNew_1[Orange].isInstanceOf[Orange] should be(true)
-    createNew_1[Apple].isInstanceOf[Apple] should be(true)
-    intercept[IllegalArgumentException] {
-      createNew_1[Banana].isInstanceOf[Banana] should be(true)
-    }
-
-//    /**
-//      * Method 2 of way to do it
-//      */
-//    implicit class ClassTagOps[T](val classTag: ClassTag[T]) extends AnyVal {
-//      def <<:(other: ClassTag[_]) = classTag.runtimeClass.isAssignableFrom(other.runtimeClass)
-//    }
+//  koan("""TypeTags can be used to instantiate types by reflection""") {
 //
-//    def createNew_2[A <: Fruit]: A = {
-//      val result = classTag[A] match {
-//        case a if a <<: classTag[Orange] => new Orange()
-//        case a if a <<: classTag[Apple] => new Apple()
+//    /**
+//      * @see http://stackoverflow.com/questions/22970209/get-typetaga-from-classa/22972751#22972751
+//      */
+//    import scala.reflect._
+//
+//    class Fruit
+//    class Orange extends Fruit
+//    class Apple extends Fruit
+//    class Banana extends Fruit
+//
+//    def createNew_1[A <: Fruit](implicit tt: TypeTag[A]) : A = {
+//
+//      val result = tt.tpe.typeSymbol.getClass match {
+//        case o1 if o1.isAssignableFrom(classOf[Orange]) => new Orange()
+//        case o2 if o2.isAssignableFrom(classOf[Apple]) => new Apple()
 //        case _ => throw new IllegalArgumentException
 //      }
 //      result.asInstanceOf[A]
 //    }
 //
-//    createNew_2[Orange].isInstanceOf[Orange] should be(true)
-//    createNew_2[Apple].isInstanceOf[Apple] should be(true)
+//    createNew_1[Orange].isInstanceOf[Orange] should be(true)
+//    createNew_1[Apple].isInstanceOf[Apple] should be(true)
 //    intercept[IllegalArgumentException] {
-//      createNew_2[Banana].isInstanceOf[Banana] should be(true)
+//      createNew_1[Banana].isInstanceOf[Banana] should be(true)
 //    }
-
-
-  }
+//
+////    /**
+////      * Method 2 of way to do it
+////      */
+////    implicit class ClassTagOps[T](val classTag: ClassTag[T]) extends AnyVal {
+////      def <<:(other: ClassTag[_]) = classTag.runtimeClass.isAssignableFrom(other.runtimeClass)
+////    }
+////
+////    def createNew_2[A <: Fruit]: A = {
+////      val result = classTag[A] match {
+////        case a if a <<: classTag[Orange] => new Orange()
+////        case a if a <<: classTag[Apple] => new Apple()
+////        case _ => throw new IllegalArgumentException
+////      }
+////      result.asInstanceOf[A]
+////    }
+////
+////    createNew_2[Orange].isInstanceOf[Orange] should be(true)
+////    createNew_2[Apple].isInstanceOf[Apple] should be(true)
+////    intercept[IllegalArgumentException] {
+////      createNew_2[Banana].isInstanceOf[Banana] should be(true)
+////    }
+//
+//
+//  }
 
 
 }
